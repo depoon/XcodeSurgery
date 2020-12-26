be6acd175460f2a1779888425addd76833f7679b
Kenneth Poon <de_poon@hotmail.com>
2020-12-27 01:24:26 +0800

    Update Package.swift
    - adding product item

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "XcodeSurgery",
+    products: [
+        .executable(name: "XcodeSurgery", targets: ["XcodeSurgery"]),
+    ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-argument-parser", from: "0.3.0"),
     ],
